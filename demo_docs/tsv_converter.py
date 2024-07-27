e8577b66a1d54cd684128c87f2ff72a5a158a682
Fangping Lan <fangpinglan0116@gmail.com>
2024-07-27 17:51:16 -0400

    script for converter and results

--- demo_docs/tsv_converter.py
+++ demo_docs/tsv_converter.py
@@ -1,9 +1,7 @@
 import os
 import re
 from collections import defaultdict
-
-
-
+import pickle
 
 def converter(main_folder):
     # Iterate through each subfolder in the main folder
@@ -26,19 +24,23 @@ def converter(main_folder):
                     print(f"Found file: {file_path}")
                     lines = read_tsv(file_path)
 
-                    sent_entities, sent_relations = get_entities_and_relations(lines)
+                    passages, passage_idx_entities_relations_mapping = get_entities_and_relations(lines)
                     print("+++++++++++++++++++++")
-                    print(sent_entities)
+                    print(passage_idx_entities_relations_mapping)
                     print("+++++++++++++++++++++")
-                    print(sent_relations)
+                    # print(sent_relations)
+                    if len(passage_idx_entities_relations_mapping) != 0:
+                        store_passages_entities_relation(doc_id, passages, passage_idx_entities_relations_mapping)
 
-                    input()
+                    # input()
 
 def read_tsv(file_path):
     with open(file_path, 'r') as f:
         return f.readlines()
 
 def get_entities_and_relations(lines):
+    passages = []
+    passage_idx_entities_relations_mapping = {}
     sent_entities = []
     sent_relations = []
     # {"label": "PER", "start": 2, "end": 4}
@@ -52,17 +54,48 @@ def get_entities_and_relations(lines):
         if len(multi_token_entity) == 0:
             value_id = None
         # print("==>", line)
+        
         if line.startswith('#') or len(line.strip()) == 0:
-            continue
+            if line.startswith("#Text"):
+                passages.append(line[6:])
+            elif len(line.strip()) == 0 and len(entity_dict) != 0:
+                print(entity_dict)
+                print(relation_dict)
+                for param_id, (value_id, relation) in relation_dict.items():
+                    (param, begin_p, end_p) = entity_dict[param_id]
+                    (value, begin_v, end_v) = entity_dict[value_id]
+                    sent_relations.append(
+                        {
+                            "label":relation,
+                            "param":param,
+                            "value":value,
+                            "begin_p":begin_p,
+                            "end_p":end_p,
+                            "begin_v":begin_v,
+                            "end_v":end_v
+                        }
+                    )
+                
+                if len(sent_entities) != 0:
+                    passage_idx_entities_relations_mapping[len(passages)-1] = (sent_entities, sent_relations)
+                
+                # initializing
+                entity_dict = {}
+                relation_dict = {}
+                sent_entities = []
+                sent_relations = []
+            else:
+                continue
         else:
             items = line.split()
+            # print(items)
             if len(items) <= 3:
                 continue
-
+            
             if items[3].strip() != '-' and items[3].strip() != '_':
-                
-                if items[3].strip().startswith('value'):
-                    if items[3].strip() == 'value':
+                print(items)
+                if items[3].strip().lower().startswith('value'):
+                    if items[3].lower().strip() == 'value':
                         # process entity
                         if value_id is None:
                             value_id = items[0].strip()
@@ -111,28 +144,37 @@ def get_entities_and_relations(lines):
                         rest_span = " ".join(items[4:])
                         print("rest_span", rest_span)
                         if '|' in rest_span:
+                            # example: 45-5	6076-6077	4	value[6]	Associated With|Associated With	44-5[0_6]|44-7[0_6]	
                             param_relation_spans = rest_span.strip().split('|')
                             for span in param_relation_spans:
                                 if '[' in span:
                                     print("span", span)
                                     param_item_id = span.split()[-1].split('[')[0].strip()
+                                    relation = " ".join(span.split()[:-1]).strip()
                                     print("param_item_id", param_item_id)
-                                    relation_dict[param_item_id] = value_id
+                                    relation_dict[param_item_id] = (value_id, relation)
                                 elif '-' in span:
+                                    # example: 74-230	11427-11430	16M	value	Associated With|Associated With	74-225|74-227
                                     print("span", span)
                                     param_item_id = span.split()[-1].strip()
+                                    relation = " ".join(span.split()[:-1])
                                     print("param_item_id", param_item_id)
-                                    relation_dict[param_item_id] = value_id
+                                    relation_dict[param_item_id] = (value_id, relation)
                         elif '[' in rest_span:
+                            # example: 5-16	2557-2560	50%	value[1]	Associated With	5-11[0_1] 
                             param_item_id = rest_span.strip().split()[-1].split('[')[0].strip()
+                            relation = " ".join(rest_span.strip().split()[:-1]).strip()
                             print("param_item_id", param_item_id)
-                            relation_dict[param_item_id] = item_id
+                            relation_dict[param_item_id] = (value_id, relation)
                         else:
+                            # example: 80-54	9920-9921	5	value	Associated With	80-52
+                            # example: 27-7	7134-7135	1	VALUE	EqualTo	27-5
                             param_item_id = rest_span.strip().split()[-1].strip()
+                            relation = " ".join(rest_span.strip().split()[:-1]).strip()
                             print("param_item_id", param_item_id)
-                            relation_dict[param_item_id] = item_id
+                            relation_dict[param_item_id] = (value_id, relation)
 
-                elif items[3].strip().startswith('parameter'):
+                elif items[3].strip().lower().startswith('param'):
                     item_id = items[0].strip()
                     index = items[1].split('-')
                     begin = int(index[0].strip())
@@ -143,7 +185,7 @@ def get_entities_and_relations(lines):
                         token = items[2].strip()[:end-begin]
                     entity_dict[item_id] = (token, begin, end)
                     print(str({
-                        "label":"value",
+                        "label":"parameter",
                         "token":token,
                         "start":begin,
                         "end":end
@@ -173,24 +215,8 @@ def get_entities_and_relations(lines):
                     )
                     multi_token_entity = []
                     value_id = None
-    print(entity_dict)
-    print(relation_dict)
-    for param_id, value_id in relation_dict.items():
-        (param, begin_p, end_p) = entity_dict[param_id]
-        (value, begin_v, end_v) = entity_dict[value_id]
-        sent_relations.append(
-            {
-                "label":"associated_with",
-                "param":param,
-                "value":value,
-                "begin_p":begin_p,
-                "end_p":end_p,
-                "begin_v":begin_v,
-                "end_v":end_v
-            }
-        )
 
-    return sent_entities, sent_relations
+    return passages, passage_idx_entities_relations_mapping
 
 def get_doc_id(folder_name):
     # Regular expression pattern to match digits in the folder names
@@ -203,8 +229,12 @@ def get_doc_id(folder_name):
         print(f"Folder: {folder_name}, Digits: {digits}")
         return digits
 
+def store_passages_entities_relation(doc_id, passages, passage_idx_entities_relations_mapping):
+    # source, destination
+    pickle.dump(passages, open(f'postgres_NER_golden/passages_entities_relations/pg{doc_id}_passages', 'wb')) 
+    pickle.dump(passage_idx_entities_relations_mapping, open(f'postgres_NER_golden/passages_entities_relations/pg{doc_id}_entities_relations', 'wb'))
 
 if __name__ == '__main__':
     # Define the path to the main folder
-    main_folder = 'mysql_NER_golden/annotation'
+    main_folder = 'postgres_NER_golden/annotation'
     converter(main_folder)
